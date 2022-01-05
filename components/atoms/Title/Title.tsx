be9bf144974c8ffb7416cce4e4c85b9263f35ae2
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2022-01-05 08:17:08 +0000

    create atom/Title

---
+++ components/atoms/Title/Title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TitleP } from "./styles";
+import styled from "styled-components";
+
+export default function Title() {
+  return (
+    <div>
+      <TitleP>People</TitleP>
+    </div>
+  );
+}
---
+++ components/atoms/Title/styles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const TitleP = styled.p`
+  font-family: "Roboto", sans-serif;
+  font-size: 4rem;
+  font-weight: 400;
+  padding-left: 50px;
+`;
